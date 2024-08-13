b97e649eb695a1778ecc0e2f608a2c2d275f4bca
jef4j <jefardu7@gmail.com>
2024-08-13 13:56:47 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'jeffForm',
+  webDir: 'www'
+};
+
+export default config;
